25888b972b93693c91dcb09b6556df0748baf046
simon-kachepa <kachepasimon@gmail.com>
2024-03-06 12:56:46 -0800

    Create base_model.py

---
+++ models/base_model.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""
+    Defining the Base Class Module
+    - Public instance attributes
+        - id- string
+        - created_at - datetime
+        - updated_at - datetime
+    - Public instant methods
+        - save(self) - updates the public instance attribute
+                        updated_at with the current datetime
+        - to_dict(self) - returns a dictionary containing all
+                        keys/values of __dict__ of the instance
+"""
+
+
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    """
+        This is the Base class that defines all common
+        attributes/methods for other classes and all
+        other classes will inherit from this class
+    """
+    
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """
+            Represent the string representation of the currect object
+        """
+
+        return ("[{}] ({}) {}"
+                .format(self.__class__.__name__, self.id, self.__dict__))
+
+    def save(self):
+        """
+            Updates the public instance attribute updated_at
+            with the current datetime
+        """
+        updated_at = datetime.now()
+
+    def to_dict(self):
+        """
+            Returns a dictionary containing all keys/values of
+            __dict__ of the instance
+        """
+        new_dictionary = self.__dict__
+
+        new_dictionary["__class__"] = self.__class__.__name__
+        new_dictionary["created_at"] = self.created_at.isoformat()
+        new_dictionary["updated_at"] = self.updated_at.isoformat()
+
+        return (new_dictionary)
