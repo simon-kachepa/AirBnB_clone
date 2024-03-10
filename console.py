17e5b9fa92ca2de3425dc37bb0745aee55fa047b
simon-kachepa <kachepasimon@gmail.com>
2024-03-10 14:05:11 -0700

    Correct console.py

--- console.py
+++ console.py
@@ -15,12 +15,21 @@ class HBNBCommand(cmd.Cmd):
     """
     prompt = "(hbnb) "
 
+     def precmd(self, line):
+        """Instructions to execute before <line> is interpreted.
+        """
+        if not line:
+            return '\n'
+
     def do_quit(self, *args):
-        """Quit command to exit the program"""
+        """Quit command to exit the program
+        """
         return (True)
 
     def do_EOF(self, *args):
-        """EOF command to exit the program"""
+        """EOF command to exit the program
+        """
+        print("")
         return (True)
 
 
