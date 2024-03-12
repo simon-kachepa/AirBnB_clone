d187295e13d5a5c130ddafdc89ed08d885bab7fb
simon-kachepa <kachepasimon@gmail.com>
2024-03-12 14:33:51 -0700

    Update console.py

--- console.py
+++ console.py
@@ -6,10 +6,28 @@
 
 
 import cmd
+import re
+from shlex import split
 from models import storage
 from models.base_model import BaseModel
 from models.user import User
 
+def parse(arg):
+    curly_braces = re.search(r"\{(.*?)\}", arg)
+    brackets = re.search(r"\[(.*?)\]", arg)
+    if curly_braces is None:
+        if brackets is None:
+            return [i.strip(",") for i in split(arg)]
+        else:
+            lexer = split(arg[:brackets.span()[0]])
+            retl = [i.strip(",") for i in lexer]
+            retl.append(brackets.group())
+            return retl
+    else:
+        lexer = split(arg[:curly_braces.span()[0]])
+        retl = [i.strip(",") for i in lexer]
+        retl.append(curly_braces.group())
+        return retl
 
 class HBNBCommand(cmd.Cmd):
     """
@@ -17,6 +35,10 @@ class HBNBCommand(cmd.Cmd):
         of the command interpreter
     """
     prompt = "(hbnb) "
+    __classes = {
+        "BaseModel",
+        "User"
+        }
 
     def do_quit(self, *args):
         """Quit command to exit the program
