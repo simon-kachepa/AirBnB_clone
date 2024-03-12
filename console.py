64fd18d7ea1fcbfbcd7e61c78ca5ad8765b1fe63
simon-kachepa <kachepasimon@gmail.com>
2024-03-12 15:18:16 -0700

    Update console.py

--- console.py
+++ console.py
@@ -11,6 +11,11 @@ from shlex import split
 from models import storage
 from models.base_model import BaseModel
 from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
 
 
 def parse(arg):
@@ -39,7 +44,12 @@ class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
     __classes = {
         "BaseModel",
-        "User"
+        "User",
+        "State",
+        "City",
+        "Place",
+        "Amenity",
+        "Review"
         }
 
     def do_quit(self, *args):
